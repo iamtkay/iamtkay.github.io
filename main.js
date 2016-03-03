9e8b515d7bfb8db0355105c80003e9ae9ad5d982
TABISH KHAN <tabish003@hotmail.com>
2016-03-02 19:56:35 -0800

    added scroll to bottom function on navbar link click, also scroll back to top link at bottom of page

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 var quotesArray = [
- "today is the most important day of my life.",
+ //"today is the most important day of my life.",
  "forever a student of the game.",
  "i'm trying to keep it alive and not compromise the feeling we love. you trying to keep it deprived and only co-sign what radio does - kendrick lamar"
 ];
 
 $(document).ready(function(){
 
+  // ========
+  // QUOTE 
+  // ========
   var numOfQuotes          = quotesArray.length;
   var randomQuoteFromArray = quotesArray[Math.floor(Math.random()*numOfQuotes)];
-
+  
   $('.main-quote').append(randomQuoteFromArray);
+
+  // =========
+  // NAVBAR 
+  // =========
+  $('.navbar > ul > li, .main-heading').click(function(){
+    $('html, body').animate({
+        scrollTop: 710
+    }, 500);
+  });
+
+  // =======================
+  // SCROLL TOP TOP TRIGGER
+  // =======================
+
+  $('.scroll-to-top-trigger').click(function(){
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+  });
 
 });
